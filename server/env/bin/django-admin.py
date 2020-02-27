e6899b3b7bedd29d6879427d212ff20bad8b575a
SmokingTheGoaT <orlando.heaply@gmail.com>
2020-02-26 18:05:56 -0700

    first commit

---
+++ server/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/orlandogarcia/Desktop/taxi-app/server/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ server/taxi/trips/tests/test_websocket.py
@@ -0,0 +1,109 @@
+from channels.testing import WebsocketCommunicator
+import pytest
+from channels.layers import get_channel_layer
+from django.contrib.auth import get_user_model
+from channels.db import database_sync_to_async
+from rest_framework_simplejwt.tokens import AccessToken
+from django.contrib.auth.models import Group
+
+from taxi.routing import application
+
+
+TEST_CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    },
+}
+
+@database_sync_to_async
+def create_user(username, password, group='rider'):
+    user = get_user_model().objects.create_user(
+        username=username,
+        password=password
+    )
+
+    user_group, _ = Group.objects.get_or_create(name=group)
+    user.groups.add(user_group)
+    user.save()
+
+    access = AccessToken.for_user(user)
+    return user, access
+
+
+@pytest.mark.asyncio 
+@pytest.mark.django_db(transaction=True)
+class TestWebSocket:
+    async def test_can_connect_to_server(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        _, access = await create_user(
+            'test.user@example.com', 'pAssword'
+        )
+        communicator = WebsocketCommunicator(
+            application=application,
+            path=f'/taxi/?token={access}'
+        )
+        connected, _ = await communicator.connect()
+        assert connected is True
+        await communicator.disconnect()
+
+    async def test_can_send_and_recieve_messages(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(
+            application=application,
+            path='/taxi'
+        )
+        connected, _ = await communicator.connect()
+        message = {
+            'type': 'echo.message',
+            'data': 'This is a test message.',
+        }
+        await communicator.send_json_to(message)
+        response = await communicator.receive_json_from()
+        assert response == message
+        await communicator.disconnect()
+
+    async def test_can_send_and_receive_broadcast_messages(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(
+            application=application,
+            path='/taxi/'
+        )
+        connected, _ = await communicator.connect()
+        message = {
+            'type': 'echo.message',
+            'data': 'This is a test message',
+        }
+        channel_layer = get_channel_layer()
+        await channel_layer.group_send('test', message=message)
+        response = await communicator.recieve_json_from()
+        assert response == message
+        await communicator.disconnect()
+
+    async def test_cannot_connect_to_socket(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(
+            application=application,
+            path='/taxi/'
+        )
+        connected, _ = await communicator.connect()
+        assert connected is False
+
+    async def test_join_driver_pool(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        _, access = await create_user(
+            'test.user@example.com', 'pAssw0rd', 'driver'
+        )
+        communicator = WebsocketCommunicator(
+            application=application,
+            path=f'/taxi/?token={access}'
+        )
+        connected, _ = await communicator.connect()
+        message = {
+            'type': 'echo.message',
+            'data': 'This is a test message.',
+        }
+        channel_layer = get_channel_layer()
+        await channel_layer.group_send('drivers', message=message)
+        response = await communicator.receive_json_from()
+        assert response == message
+        await communicator.disconnect()
